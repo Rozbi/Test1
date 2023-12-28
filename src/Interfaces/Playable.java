26e402fd5a899effdd8280fa87c875e1921aa4a7
Rozbi <153735526+Rozbi@users.noreply.github.com>
2023-12-28 17:06:42 +0300

    Playable.java

--- src/Interfaces/Playable.java
+++ src/Interfaces/Playable.java
@@ -5,5 +5,5 @@ import Objects.Game;
 import Objects.Person;
 
 public abstract interface Playable {
-    public String play(int point, Status stat, Game g);
+    public void play(int point, Status stat, Game g);
 }
